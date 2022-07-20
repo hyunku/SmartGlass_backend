709a0cb7b9f6b2cfaa9b4004cac373fdf4e25538
kahk <kahk000@naver.com>
2022-07-20 15:49:20 +0900

    rename table

--- detectUS/listapp/models.py
+++ detectUS/listapp/models.py
@@ -5,16 +5,20 @@ from django.db.models import CASCADE
 # Create your models here.
 class Company(models.Model):
     company_id = models.BigAutoField(primary_key=True)
-    company_name = models.CharField(max_length=64) # 문자열 필드 (최대길이 : 64)
+    company_name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'company'
+
 
 class Building(models.Model):
-    company_id = models.ForeignKey(Company, on_delete=CASCADE, db_column='company_id') # 외래키 지정
-    building_name = models.TextField(null=False) #null값 존재여부
+    company_id = models.ForeignKey(Company, on_delete=CASCADE, db_column='company_id')  # 외래키 지정
+    building_name = models.TextField(null=False)  # null값 존재여부
     building_id = models.BigAutoField(primary_key=True)
     max_floor = models.IntegerField(null=True)
     min_floor = models.IntegerField(null=True)
     context = models.CharField(max_length=1024)
 
     class Meta:
-        ordering = ['-max_floor'] # db정렬 방식 -> -붙으면 내림차순 (max_floor에 대한 내림차순 정렬)
-
+        db_table = 'building'
+        ordering = ['-max_floor']  # db정렬 방식 -> -붙으면 내림차순 (max_floor에 대한 내림차순 정렬)
