41f5950771b9076c12f7c4c86ea33e026ed4765f
최혜린 <lin419@likelion.org>
2022-08-15 01:45:57 +0900

    accountsapp 로그인,회원가입,로그아웃

--- detectUS/accounts/urls.py
+++ detectUS/accounts/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
-from .views import signupAPIView 
+from .views import *
 from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'accounts'
 
 urlpatterns = [
   #  path('', views.index, name='accounts'),
-  path('signup', signupAPIView.as_view()),
+  path('signup',views.Sign_up,name="Signup"),
+  path('login',views.login,name="Login"),
+  path('logout/<str:user_id>',views.logout,name="Logout"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- detectUS/accounts/views.py
+++ detectUS/accounts/views.py
@@ -1,44 +1,100 @@
+
+from ast import Return
 import json
+from msilib.schema import Class
+from re import template
+from telnetlib import STATUS
+from home.models import Company, Account
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
-from rest_framework import status
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from home.models import Account,Company
-from .serializers import companySerializer, signupSerializer
 
-#create
-from django.contrib.auth.models import User
-from rest_framework.validators import UniqueValidator
-
-# Create your views here.
-class signupAPIView(APIView):
-    permission_classes = (AllowAny,)
-    serializer_class = signupSerializer
-   
-    def get(self, request,*args, **kwargs):
-        queryset = Account.objects.all() 
-        print(queryset)
-        serializer = signupSerializer(many=True)
-        return Response(serializer.data)
-
+@csrf_exempt
+def Sign_up(request) :
     
-    
-    def post(self, request,*args, **kwargs):
-        
-        serializer = self.serializer_class(data=request.data)
-        extra_kwargs = {
-        'user_id': {
-            'validators': [UniqueValidator(queryset=User.objects.all())]
-        },
+    response_data ={
+        "code": 200,
+        "message": "회원가입에 성공하였습니다."
         }
-
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=200)
-        return Response(data=serializer.data, status=400)
     
-  
+     #데이터중 company_name 찾기
+    if request.method=="POST":
+        #json , 받아오는 방법 loads
+        received_json_data = json.loads(request.body.decode("utf-8"))
+      #받아온 json을 확인 할 수 있음   print(received_json_data)
+      #대소문자를 가리지 않고 comapany_name을 받아올 것이기 때문에 iexact
+        Be_company = Company.objects.filter(company_name__iexact=received_json_data["company_name"]).values('company_name')
+        Be_user_id = Account.objects.filter(user_id__iexact=received_json_data["user_id"]).values("user_id")
+        print( Be_company)
+        print(received_json_data["company_name"])
+        #filter만 했을때 <QuerySet [<Company: Company object (1)>]>
+        #values까지 했을때 <QuerySet [{'company_name': '한이음'}]>
+        
+        
+        #company name이 동일 한 것이 없을 때 
+        if Be_company.first()==None :
+            Company.objects.create(company_name=received_json_data["company_name"])
+            company_id_Query =Company.objects.filter(company_name__iexact=received_json_data["company_name"]).prefetch_related('company').values('company_id')
+            a=company_id_Query.first().get('company_id')
+            #<QuerySet [{'company_id': 1}]>
+            #Company object (1) instance
+            company_id_instance= Company.objects.filter(company_id=a)
+            company_id_instance=company_id_instance.first()
+            received_json_data.pop('company_name')
+            Account.objects.create(company_id=company_id_instance,**received_json_data)
+            print("company_name 생성 후 회원가입 완료 ")
+            return JsonResponse(response_data, status=200)
+            #id 조회해서 가져오기 
+        #company name이 동일 한 것이 있을 때 
+        else : 
+            if Be_user_id.first()==None :
+                print("원래 company_name조회하여 company_id 가져옴")
+                company_id_Query =Company.objects.filter(company_name__iexact=received_json_data["company_name"]).prefetch_related('company').values('company_id')
+                a=company_id_Query.first().get('company_id')
+                company_id_instance= Company.objects.filter(company_id=a)
+                company_id_instance=company_id_instance.first()
+                received_json_data.pop('company_name')
+                Account.objects.create(company_id=company_id_instance,**received_json_data)
+                print("회원가입 완료 ")
+                return JsonResponse(response_data, status=200)
+            else :
+              return JsonResponse({'message' : 'user_id가 중복되었습니다.'}, status=400)
+         
+    elif request.method=="GET":
+        data= Account.objects.all() 
+        return JsonResponse({'message':'get'}, status=200)
+    
+@csrf_exempt
+def login(request) :
+    response_data ={
+        "code": 200,
+        "message": "로그인에 성공하였습니다."
+        }
+    
+     #데이터중 company_name 찾기
+    if request.method=="POST":
+        #json , 받아오는 방법 loads
+        received_json_data = json.loads(request.body.decode("utf-8"))
+        user_id=Account.objects.filter(user_id__iexact=received_json_data["user_id"]).values("user_id")
+        user_pw=Account.objects.filter(user_pw__iexact=received_json_data["user_pw"]).values("user_pw")
+        
+        if user_id.first()==None :
+             return JsonResponse({'message':'user_id가 존재하지 않습니다.'}, status=200)
+        else :
+            if user_pw.first()==None:
+                return JsonResponse({'message':'비밀번호가 틀리셨습니다.'}, status=200)   
+            else :
+                return JsonResponse(response_data, status=200)
+            
+    elif request.method=="GET":
+        return JsonResponse({'message':'login api입니다.'}, status=200)
+    
+@csrf_exempt
+def logout(request,user_id) :
+    if request.method=="GET":
+        user_id=Account.objects.filter(user_id__iexact=user_id).values("user_id")
+        if user_id.first()==None :
+            return JsonResponse({'message':'user_id를 다시 입력해주세요.'}, status=200)
+        else :
+            return JsonResponse({'message':'로그아웃 되었습니다.'}, status=200)
